9aa306c508acecc9f5ba0504db3cb78ef7efe88f
Dhruv Gupta <guptadhruv2803@gmail.com>
2020-11-20 11:46:12 +0530

    iconSwitcher fixed

--- src/background/IconSwitcher.js
+++ src/background/IconSwitcher.js
@@ -1,9 +1,14 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.action === "updateIcon") {
         if (request.value) {
-            chrome.browserAction.setIcon({path: "logo/square 16 bw.png"},null);
+            chrome.browserAction.setIcon({
+                path: "logo/square 16 bw.png",
+                tabId: sender.tab.id
+            });
         } else {
-            chrome.browserAction.setIcon({path: "logo/Square 16.png"},null);
+            chrome.browserAction.setIcon({
+                path: "logo/Square 16.png",
+                tabId: sender.tab.id});
         }
     }
-})
+});
